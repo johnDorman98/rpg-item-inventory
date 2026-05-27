c2a057005c9d754c260ae53c019c400763ec90a7
John Dorman <namrods15@gmail.com>
2026-05-27 23:37:46 +0200

    refactor: enhance HTML structure and implement item management in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello from script.js")
+// Represents a single piece of gear in the player's inventory
+function Item(name, type, quantity) {
+  this.id = crypto.randomUUID();
+  this.name = name;
+  this.type = type;
+  this.quantity = quantity;
+}
+
+// Reductions when a player consumes or equips an item
+Item.prototype.useItem = function () {
+  this.quantity -= 1;
+}
+
+const inventoryItems = [];
+
+// Instantiates a new item and adds it to the master collection
+function addItemToInventory(name, type, quantity) {
+  const newItem = new Item(name, type, quantity);
+  inventoryItems.push(newItem);
+}
