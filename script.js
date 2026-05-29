24fbfceb6c41cc5d71c4cfdb9c0d823c40905b96
John Dorman <namrods15@gmail.com>
2026-05-29 22:46:59 +0200

    refactor: improve form structure and enhance item addition functionality

--- script.js
+++ script.js
@@ -9,12 +9,40 @@ function Item(name, type, quantity) {
 // Reductions when a player consumes or equips an item
 Item.prototype.useItem = function () {
   this.quantity -= 1;
-}
-
-const inventoryItems = [];
+};
 
 // Instantiates a new item and adds it to the master collection
 function addItemToInventory(name, type, quantity) {
   const newItem = new Item(name, type, quantity);
   inventoryItems.push(newItem);
 }
+
+function displayInventory(inventory) {
+  const inventoryContainer = document.querySelector(".inventory-container");
+  inventoryContainer.textContent = "";
+
+  inventory.forEach(item => {
+    const inventoryCard = document.createElement("div");
+    inventoryCard.classList.add("inventory-card")
+  });
+}
+
+const inventoryItems = [];
+
+// Handle form submissions.
+const form = document.querySelector("#add-item-form");
+
+form.addEventListener("submit", (event) => {
+  // Prevent default behavior
+  event.preventDefault();
+
+  // Extract values from submitted form.
+  const newItemName = event.target.elements["new-item-name"].value;
+  const newItemType = event.target.elements["new-item-type"].value;
+  const newItemQuantity = Number(
+    event.target.elements["new-item-quantity"].value,
+  );
+
+  // Create new Item object
+  addItemToInventory(newItemName, newItemType, newItemQuantity);
+});
