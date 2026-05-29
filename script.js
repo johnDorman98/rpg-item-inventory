9a54db279b4976f5068e6d561947e0f700ad2b1b
John Dorman <namrods15@gmail.com>
2026-05-29 23:12:48 +0200

    refactor: enhance displayInventory function to include item type and quantity

--- script.js
+++ script.js
@@ -17,13 +17,31 @@ function addItemToInventory(name, type, quantity) {
   inventoryItems.push(newItem);
 }
 
+// Updates inventoryContainer with a card containing data for each inventory item.
 function displayInventory(inventory) {
+  // Target the grid container and wipe it to prevent duplicate cards
   const inventoryContainer = document.querySelector(".inventory-container");
   inventoryContainer.textContent = "";
 
   inventory.forEach(item => {
+    // Create a card for each inventory item.
     const inventoryCard = document.createElement("div");
     inventoryCard.classList.add("inventory-card")
+
+    // Create elements to be added to inventory card
+    const itemNameElement = document.createElement("h3");
+    itemNameElement.textContent = item.name;
+    inventoryCard.appendChild(itemNameElement);
+
+    const itemTypeElement = document.createElement("p");
+    itemTypeElement.textContent = item.type;
+    inventoryCard.appendChild(itemTypeElement);
+    
+    const itemQuantityElement = document.createElement("p");
+    itemQuantityElement.textContent = item.quantity;
+    inventoryCard.appendChild(itemQuantityElement);
+
+    inventoryContainer.appendChild(inventoryCard);
   });
 }
 
@@ -45,4 +63,7 @@ form.addEventListener("submit", (event) => {
 
   // Create new Item object
   addItemToInventory(newItemName, newItemType, newItemQuantity);
+
+  // Clear and rebuild the visible inventory list on the screen
+  displayInventory(inventoryItems);
 });
